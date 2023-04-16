2b5a3207b6eb078e2cf1c151371342366b074030
nineosu <nineosu@gmail.com>
2023-04-16 20:08:28 +0500

    added new pages

--- js/main.js
+++ js/main.js
@@ -1,36 +1,61 @@
-const swiper1 = new Swiper('.swiper1', {
-    // Optional parameters
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    autoHeight: true,
-  
-});
+if (document.querySelector('.swiper1')) {
+    const swiper1 = new Swiper('.swiper1', {
+        // Optional parameters
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoHeight: true,
+      
+    });
+}
 
-const swiper2 = new Swiper('.swiper2', {
-    // Optional parameters
-    spaceBetween: 30,
-    loop: true,
-    allowSlidePrev: false,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next2',
-    },
-  
-});
+if (document.querySelector('.swiper2')) {
+    const swiper2 = new Swiper('.swiper2', {
+        // Optional parameters
+        slidesToShow: 1,
+        spaceBetween: 30,
+        loop: true,
+        allowSlidePrev: false,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next2',
+        },
+      
+    });
+}
+
+if (document.querySelector('.swiper3')) {
+    const swiper3 = new Swiper('.swiper3', {
+        // Optional parameters
+        slidesToShow: 1,
+        spaceBetween: 30,
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          prevEl: '.swiper-button-prev3',
+          nextEl: '.swiper-button-next3',
+        },
+      
+    });
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     // Burger
     const burger = document.querySelector('.burger'),
           navBar = document.querySelector('.header__nav'),
           headerList = document.querySelector('.header__list'),
-          closeBurger = document.querySelector('.header__close');
+          listItems = document.querySelectorAll('.header__list-item'),
+          subLists = document.querySelectorAll('.header__list-sublist'),
+          closeBurger = document.querySelector('.header__close'),
+          sliders = document.querySelectorAll('.swiper');
 
     burger.addEventListener('click', () => {
         navBar.classList.toggle('header__nav-active');
         headerList.classList.toggle('header__list-active');
         closeBurger.style.display = 'block';
+        sliders.forEach(item => item.style.display = 'none');
         document.body.style.overflow = 'hidden';
         document.body.style.position = 'fixed';
     });
@@ -39,7 +64,20 @@ window.addEventListener('DOMContentLoaded', () => {
         navBar.classList.toggle('header__nav-active');
         headerList.classList.toggle('header__list-active');
         closeBurger.style.display = 'none';
+        sliders.forEach(item => item.style.display = '');
         document.body.style.overflow = '';
         document.body.style.position = '';
     })
+
+    listItems.forEach(item => {
+        if (item.getAttribute('data-sublist')) {
+            item.addEventListener('click', () => {
+                subLists.forEach(list => {
+                    if (item.contains(list)) {
+                        list.classList.toggle('sublist-show');
+                    }
+                });
+            });
+        } 
+    });
 });
